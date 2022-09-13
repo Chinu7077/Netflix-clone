02b68e58df2611a3597315051f5bb65b68d72bf3
Chinu7077 <97742083+Chinu7077@users.noreply.github.com>
2022-09-13 15:44:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Row from "./Row";
+import requests from "./requests";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
